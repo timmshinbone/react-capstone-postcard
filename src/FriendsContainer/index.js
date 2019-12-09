facd5359e3930f2260bd84ee9810c662547dbf1e
Timm Schoenborn <timmschoenborn@gmail.com>
2019-12-08 20:22:12 -0600

    can display list of users friends

--- src/FriendsContainer/index.js
+++ src/FriendsContainer/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Button, Label, } from 'semantic-ui-react';
+import { List, Icon, Header, Button } from 'semantic-ui-react';
 
 function FriendsContainer(props){
 	// get a list of friendships for the current user
@@ -7,13 +7,35 @@ function FriendsContainer(props){
 		friends.user_one.username === props.loggedInUsername
 		||
 		friends.user_two.username === props.loggedInUsername)
-	console.log(currentFriendships);
+	console.log(currentFriendships, "<--currentFriendships");
 	//display friends where status is 1(accepted)
+	const friends = currentFriendships.filter((req) => req.status === 1)
+	console.log(friends, "<-This is friends");
+
+	const friendsNames = friends.reduce((arr, person) => {
+		if(person.user_one.username !== props.loggedInUsername){
+			return arr.concat(person.user_one.username)
+		} else if(person.user_two.username !== props.loggedInUsername){
+			return arr.concat(person.user_two.username)
+		} else {
+			return arr
+		}
+	}, [])
+	console.log(friendsNames, '<--this is friendsNames');
+	const friendList = friendsNames.map((fren) => {
+		return(
+			<List.Item key={fren.id}>
+				<List.Header><Icon name='user outline'/>{fren}</List.Header>
+			</List.Item>
+		)
+	})
 	//display pending requests 
 	//allow for modal to add friends
 
 	return(
-		<p>Friends</p>
+		<List>
+			{ friendList }
+		</List>
 	)
 }
 
